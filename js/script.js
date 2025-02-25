3e0f0c31c22ccd15c37bb8fdd4250b343794bf8a
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-02-25 14:42:42 +0100

    feat: add conditions "FizzBuzz"

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 // Stampare numeri da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+
+    // Controllo mutiplo 3 e 5
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        // Controllo mutiplo 3
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        // Controllo mutiplo 5
+        console.log("Buzz");
+    } else {
+        // Stampare il numero
+        console.log(i);
+    }
 
 }
